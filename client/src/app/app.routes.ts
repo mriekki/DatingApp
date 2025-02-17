617a8476ae2f9e5614cdac8941e2ebe761bddf22
Mark Riekki <mriekki@users.noreply.github.com>
2025-02-17 13:55:31 -0800

    Prior to Angular upgrade section 9

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -16,8 +16,8 @@ export const routes: Routes = [
         runGuardsAndResolvers: 'always',
         canActivate: [authGuard],
         children: [
-            {path: 'members', component: MemberListComponent, canActivate: [authGuard]},    
-            {path: 'members/:id', component: MemberDetailComponent},    
+            {path: 'members', component: MemberListComponent},    
+            {path: 'members/:username', component: MemberDetailComponent},    
             {path: 'lists', component: ListsComponent},    
             {path: 'messages', component: MessagesComponent},
         ]
